487d49041f56f97eb3714be27063f92fe3d3747a
Ahmed4726 <ma7985067@gmail.com>
2025-03-13 17:49:39 +0500

    Update f_layout.blade.php

--- resources/views/layouts/f_layout.blade.php
+++ resources/views/layouts/f_layout.blade.php
@@ -15,7 +15,9 @@
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+            @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
             <!-- Page Content -->
